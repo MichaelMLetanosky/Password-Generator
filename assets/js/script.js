0a13b4ba261e9b804752f07e193ead379797d5f6
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-03-30 20:41:56 -0400

    Code Refactor 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Arrays for special characters, lowercase letters, uppercase letters, and numbers
 const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
 
@@ -74,14 +85,3 @@ function generatePassword() {
 
   return finalPassword;
 }
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
